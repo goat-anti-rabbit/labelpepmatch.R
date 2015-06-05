e1aa9bc3372ea6a06cbb988444b06aa38b2d2a7a
goat-anti-rabbit <rik.verdonck@bio.kuleuven.be>
2015-06-05 13:56:20 +0200

    worm data added

---
+++ R/wormdata.R
@@ -0,0 +1,9 @@
+#' Dataset with TMAB labelled worm peptides. 
+#' @details These are peptides from entire body homogenates of adult (\emph{Caenorhabditis elegans}) worms labelled with light (D0) and heavy (D9) TMAB. The conditions are N2 and AEX5: Bristol N2 wild type, and aex5 mutants. Check the design matrix \code{wormdata$design} to have an overview of the labeling. 
+#' @name wormdata
+#' @references Will be published soon. 
+#' @docType data
+#' @author Wouter Dehaes
+#' @keywords data worm caenorhabditis elegans nematode example 
+"wormdata"
+
